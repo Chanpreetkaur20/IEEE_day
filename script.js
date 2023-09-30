3dca4abce3d81750da6d7f1b000c4d23113e660e
Harjot Singh <harjjotsinghh@gmail.com>
2023-09-30 23:44:14 +0530

    master -> main

--- script.js
+++ script.js
@@ -16,3 +16,47 @@ buttons.forEach( button =>{
         icon.classList.toggle('rotate');
     })
 } )
+
+// on scroll fade in effect
+function reveal() {
+  var reveals = document.querySelectorAll(".question , .devcont");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("visible");
+    } else {
+      reveals[i].classList.remove("visible");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
+// on scroll timeline
+var items = document.querySelectorAll(".timeline li");
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      if(!items[i].classList.contains("in-view")){
+        items[i].classList.add("in-view");
+      }
+    } else if(items[i].classList.contains("in-view")) {
+        items[i].classList.remove("in-view");
+    }
+  }
+}
+ 
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
